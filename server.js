380cb26966b09d6a63853a0bb950d8da91611872
Charan Reddy <charanedula@gmail.com>
2026-02-17 18:34:44 +0530

    simple server

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
 
 const app = express();
-app.use(express.json());
-app.use(cors());
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-// PRODUCTS ROUTE
-app.get("/products", async (req, res) => {
+app.get("/products", (req, res) => {
   res.json([]);
 });
 
 app.listen(5000, () => {
-  console.log("Server started on port 5000");
+  console.log("Server started");
 });
